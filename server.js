8295b4f7767118337c88a293d773fcc729dde1ad
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-08-25 16:41:41 +0300

    start using http2

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const FS = require('fs');
+const Http2 = require('http2');
+const Path = require('path');
+
+// read certificate and private key
+const serverOptions = {
+    key: FS.readFileSync(Path.resolve(__dirname, 'certs/myKey.key')),
+    cert: FS.readFileSync(Path.resolve(__dirname, 'certs/myCertificate.crt')),
+    allowHTTP1: true
+};
+
+// create http2 secure server listener
+const listener = Http2.createSecureServer(serverOptions);
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        tls: true,
+        listener
     });
 
     server.route({
