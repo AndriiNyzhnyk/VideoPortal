89683cc7e34202f0cd4ba7eb08c33f79d9c3b506
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-08-26 22:58:39 +0300

    add validator for payload

--- api/signUp/routes/index.js
+++ api/signUp/routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Joi = require('@hapi/joi');
 const controllers = require('../controllers');
 
 module.exports = [
@@ -11,7 +12,18 @@ module.exports = [
     {
         method: 'POST',
         path: '/registration',
-        handler: controllers.registration
+        handler: controllers.registration,
+        options: {
+            validate: {
+                payload: {
+                    userName: Joi.string().min(1).max(140),
+                    email: Joi.string().email(),
+                    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+                    confirmPassword: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+
+                }
+            }
+        }
     }
 
 ];
