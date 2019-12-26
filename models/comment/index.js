617f2bb15005abc4252f5dfa1f64083fa52dd537
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-12-26 13:27:15 +0200

    edit comments for func 'addNewComment'

--- models/comment/index.js
+++ models/comment/index.js
@@ -4,8 +4,8 @@ const self = module.exports = {
 
     /**
      * Add new comment to DB
-     * @param {objectId} author Reference to author this post
-     * @param {objectId} movie Reference to movie this post
+     * @param {objectId} author Reference to author this post(comment)
+     * @param {objectId} movie Reference to movie this post(comment)
      * @param {string} text Body of the comment
      * @returns {Promise<*>} New comment
      */
