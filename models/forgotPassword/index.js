acd2b4323a7e1f5724f8d8b549fd56fce9db0312
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-12-29 14:17:08 +0200

    add comments for model 'ForgotPassword'

--- models/forgotPassword/index.js
+++ models/forgotPassword/index.js
@@ -3,11 +3,21 @@
 const ForgotPassword = require('./ForgotPassword');
 
 const self = module.exports = {
-
+    /**
+     * Find one 'Forgot Password' entry
+     * @param {object} filter Filter compatible with MongoDB
+     * @returns {Promise<*>}
+     */
     findOne: (filter) => {
         return ForgotPassword.findOne(filter);
     },
 
+    /**
+     * Create new Forgot Password' entry
+     * @param {string} userId User '_id'
+     * @param {string} verifyCode Simple random verify code
+     * @returns {Promise<*>}
+     */
     createForgotPasswordEntry: (userId, verifyCode) => {
         return ForgotPassword.create({
             userId,
