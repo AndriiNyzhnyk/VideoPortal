1a37328d22b206a96400c61906501be3f9654442
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-01-18 18:05:45 +0200

    add comments to Movie methods

--- models/movie/index.js
+++ models/movie/index.js
@@ -3,10 +3,21 @@
 const Movie = require('./Movie');
 
 const self = module.exports = {
+    /**
+     * Create new entry into DB
+     * @param {Object} payload
+     * @returns {Promise<Object>}
+     */
     addNewMovieToDb: async (payload) => {
         return Movie.create(payload);
     },
 
+    /**
+     * Add new comment to movie
+     * @param {String} movieId
+     * @param {String} commentId
+     * @returns {Promise<Object>}
+     */
     addNewCommentToMovie: async (movieId, commentId) => {
         return Movie.findByIdAndUpdate(movieId, { $push: { comments: commentId }});
     }
