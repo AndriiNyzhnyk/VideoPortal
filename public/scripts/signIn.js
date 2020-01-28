e3aaba789deea11b2c3bddb5c083d2cc1a74f911
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-01-28 22:13:06 +0200

    add additional logic to SignIn page (add tokens to localStorage and other)

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -3,37 +3,54 @@
 const form = document.getElementById('form');
 form.addEventListener('submit', submitForm);
 
-function submitForm(e) {
-    e.preventDefault();
+async function submitForm(e) {
+    try {
+        e.preventDefault();
 
-    const userName = document.getElementById('uname').value;
-    const password = document.getElementById('psw').value;
+        const userName = document.getElementById('uname').value;
+        const password = document.getElementById('psw').value;
 
-    if (userName !== '' && password !== '') {
-        sendForm({userName, password});
-    } else {
-        alert('Please input all field');
+        if (userName !== '' && password !== '') {
+            const response = await sendForm({userName, password});
+            const body = await response.json();
+
+            if (response.status === 200) {
+
+                await addTokenToStorage(body);
+
+                if (response.redirected) {
+                    window.location.replace(response.url)
+                }
+
+                window.location.replace('https://localhost:8080/');
+            }
+
+            alert('Wrong credentials');
+            form.reset();
+        } else {
+            alert('Please input all field');
+        }
+    } catch (err) {
+        console.error(err);
     }
 }
 
 async function sendForm(data) {
-    try {
-        const url = '/login';
-        const queryString = `userName=${data.userName}&password=${data.password}`;
+    const url = '/login';
+    const queryString = `userName=${data.userName}&password=${data.password}`;
 
-        const options = {
-            method: 'post',
-            headers: {
-                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
-            body: queryString
-        };
+    const options = {
+        method: 'post',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
+        body: queryString
+    };
 
-        const response = await fetch(url, options);
+    return fetch(url, options);
+}
 
-        if (response.redirected) {
-            window.location.replace(response.url)
-        }
-    } catch (err) {
-        console.error('Request failed', err)
+async function addTokenToStorage(data) {
+    for (let key in data) {
+        localStorage.setItem(key, data[key]);
     }
 }
