837778599b9259244e442ec2d57c116343ed34b4
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-02-26 21:46:08 +0200

    add logic for adding user comment about movie

---
+++ public/scripts/moviePage.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const form = document.getElementById('newCommentForm');
+form.addEventListener('submit', submitForm);
+
+async function submitForm(e) {
+    try {
+        e.preventDefault();
+
+        const movieId = document.getElementById('movieId').value;
+        const comment = document.getElementById('comment').value;
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiNDJhNWVjYmNkZDRhZWVjNmVjZTE4NDA0Y2IxZDcxMmQiLCJ1c2VySWQiOiIyMGZlNjg4MmM5OTA3OTY4YTFlMDcyYmI4ZTQ0MmI4MzoxNjRiYWI5YzJmNGYwZGYxN2ViOWIyYTAxNDg2ZDNkMWUxZjMxNGNmNzkzNjY2ZDA0MDY4ZDRmMWVmNzVkOTZlIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTU4Mjc0MzA4OCwiZXhwIjoxNTgyNzQ2Njg4fQ.8mdKkVZZ4_G96E2fB6UF_ZaDQ7qv_Cm_e1MgZFbXxuI';
+
+
+        if (movieId === '' || comment === '') {
+            alert("Wrong comment");
+        }
+
+
+        const response = await sendForm(movieId, comment, token);
+        const body = await response.json();
+
+        if (response.status === 200) {
+            console.log('200');
+        }
+
+        // alert('Wrong credentials');
+        // form.reset();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function sendForm(movieId, comment, token) {
+    const url = '/comment';
+    const queryString = `movieId=${movieId}&comment=${comment}`;
+
+    const options = {
+        method: 'post',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Authorization": token // 'Bearer ' + token
+        },
+        body: queryString
+    };
+
+    return fetch(url, options);
+}
