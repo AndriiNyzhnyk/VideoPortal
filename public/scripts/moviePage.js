d63f3a78eaa7fee416e35654d1a20601dda4cd3d
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-02-29 15:54:54 +0200

    Read response(comment) and then render new comment on the movie page

--- public/scripts/moviePage.js
+++ public/scripts/moviePage.js
@@ -11,23 +11,25 @@ async function submitForm(e) {
         const comment = document.getElementById('comment').value;
         // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiNDJhNWVjYmNkZDRhZWVjNmVjZTE4NDA0Y2IxZDcxMmQiLCJ1c2VySWQiOiIyMGZlNjg4MmM5OTA3OTY4YTFlMDcyYmI4ZTQ0MmI4MzoxNjRiYWI5YzJmNGYwZGYxN2ViOWIyYTAxNDg2ZDNkMWUxZjMxNGNmNzkzNjY2ZDA0MDY4ZDRmMWVmNzVkOTZlIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTU4Mjc0MzA4OCwiZXhwIjoxNTgyNzQ2Njg4fQ.8mdKkVZZ4_G96E2fB6UF_ZaDQ7qv_Cm_e1MgZFbXxuI';
         const token = getAccessToken();
-        console.log(token);
 
         if (movieId === '' || comment === '') {
             alert("Wrong comment");
         }
 
         const response = await sendForm(movieId, comment, token);
-        // const body = await response.json();
 
-       if (response.status === 401) {
+        if (response.status === 401) {
            const redirectSignIn = confirm("Щоб додати коментар потрібно увійти у систему! Здійснити перехід на сторінку входу");
 
            if (redirectSignIn) {
                rememberCurrentPage();
                window.location.replace(`${window.location.origin}/sign-in`);
            }
-       }
+        }
+
+        // Read response (comment) and then render new comment on the movie page
+        const body = await response.json();
+        renderNewComment(body);
 
         form.reset();
     } catch (err) {
@@ -57,4 +59,37 @@ function getAccessToken() {
 
 function rememberCurrentPage() {
     window.localStorage.setItem('moviePage', window.location.href);
+}
+
+function renderNewComment(comment) {
+    // <div class="comment" id="content_{{this._id}}">
+    //     <div class="comment__header"><span>Posted: {{this.posted}}</span></div>
+    //     <div class="comment__text">{{this.text}}</div>
+    // </div>
+
+    const comments = document.getElementById('comments');
+
+    let father = document.createElement('div');
+    father.className = 'comment';
+    father.id = `comment_${comment._id}`;
+
+
+    let child1 = document.createElement('div');
+    child1.className = 'comment__header';
+
+    let span = document.createElement('span');
+    const posted = document.createTextNode(`Posted: ${comment.posted}`);
+    span.append(posted);
+    child1.append(span);
+
+
+    let child2 = document.createElement('div');
+    child2.className = 'comment__text';
+    const commentText = document.createTextNode(comment.text);
+    child2.append(commentText);
+
+    father.append(child1);
+    father.append(child2);
+
+    comments.append(father);
 }
