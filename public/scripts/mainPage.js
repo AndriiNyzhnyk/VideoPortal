ee117a6cfc4d162850c151a62fa806cff22b6643
Andrii Nyzhnyk <andrij1510@gmail.com>
2020-03-21 14:03:15 +0200

    split logic related to search input on main page and movie page

--- public/scripts/mainPage.js
+++ public/scripts/mainPage.js
@@ -132,29 +132,4 @@ window.onload = function () {
         // console.log('resize');
         setWidth();
     });
-
-
-    /**
-     * Logic related to search movie
-     */
-    const searchInput = document.getElementById("searchInput");
-    const searchIcon = document.getElementById("searchIcon");
-
-    searchIcon.addEventListener("click", (e) => {
-        const searchText = searchInput.value;
-
-        if (searchText) {
-            window.location.href = `movie/search/${searchInput.value}`;
-        }
-    });
-
-    // Execute a function when the user releases a key on the keyboard
-    searchInput.addEventListener("keyup", (e) => {
-        // Number 13 is the "Enter" key on the keyboard
-        if (e.keyCode === 13) {
-
-            // Trigger the button element with a click
-            searchIcon.click();
-        }
-    });
 };
